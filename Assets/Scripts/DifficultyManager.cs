34b88ed992bace50895c4c7a0914f3fbf371a928
Jaiden Wahjad <jaidenwahjad@gmail.com>
2026-01-24 15:11:23 +0900

    Restructure DifficultyManager

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -8,35 +8,69 @@ public class DifficultyManager : MonoBehaviour
     [SerializeField] private int speedIncreaseAmount = 5;
     [SerializeField] private float spawnTimeDecreaseAmount = 0.3f;
 
+    private TrainController _trainController;
+    private ObjectsSpawner _objectsSpawner;
+
+    private void Awake()
+    {
+        _trainController = FindFirstObjectByType<TrainController>();
+        _objectsSpawner = FindFirstObjectByType<ObjectsSpawner>();
+    }
+
     public void IncreaseDifficulty(int score)
     {
-        if (score % speedIncreaseThreshold == 0)
+        if (ShouldIncreaseSpeed(score))
         {
-            // Increase train speed
-            TrainController trainController = FindFirstObjectByType<TrainController>();
-            if (trainController != null)
-            {
-                trainController.IncreaseSpeed(speedIncreaseAmount);
-            }
+            IncreaseTrainSpeed();
         }
 
-        if (score % spawnRateIncreaseThreshold == 0)
+        if (ShouldIncreaseSpawnRate(score))
         {
-            // Increase obstacle spawn rate
-            ObjectsSpawner objectsSpawner = FindFirstObjectByType<ObjectsSpawner>();
-            if (objectsSpawner != null)
-            {
-                objectsSpawner.DecreaseSpawnTime(spawnTimeDecreaseAmount);
-            }
+            IncreaseObstacleSpawnRate();
         }
 
-        if (score % spawnAmountIncreaseThreshold == 0)
+        if (ShouldIncreaseSpawnAmount(score))
         {
-            ObjectsSpawner objectsSpawner = FindFirstObjectByType<ObjectsSpawner>();
-            if (objectsSpawner != null)
-            {
-                objectsSpawner.objectsToSpawn++;
-            }
+            IncreaseObstacleSpawnAmount();
+        }
+    }
+
+    private bool ShouldIncreaseSpeed(int score)
+    {
+        return score % speedIncreaseThreshold == 0;
+    }
+
+    private bool ShouldIncreaseSpawnRate(int score)
+    {
+        return score % spawnRateIncreaseThreshold == 0;
+    }
+
+    private bool ShouldIncreaseSpawnAmount(int score)
+    {
+        return score % spawnAmountIncreaseThreshold == 0;
+    }
+
+    private void IncreaseTrainSpeed()
+    {
+        if (_trainController != null)
+        {
+            _trainController.IncreaseSpeed(speedIncreaseAmount);
+        }
+    }
+
+    private void IncreaseObstacleSpawnRate()
+    {
+        if (_objectsSpawner != null)
+        {
+            _objectsSpawner.DecreaseSpawnTime(spawnTimeDecreaseAmount);
+        }
+    }
+
+    private void IncreaseObstacleSpawnAmount()
+    {
+        if (_objectsSpawner != null)
+        {
+            _objectsSpawner.objectsToSpawn++;
         }
     }
 }
